a34e3621ea6012709b087a0b7ae08a90d87af757
Arthur Biasibetti Farias <arthurbiasibettifarias@gmail.com>
2019-07-13 14:53:27 -0300

    teste

---
+++ src/pages/Aluno/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#271B3D',
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        paddingHorizontal: 20,
+    }
+});
