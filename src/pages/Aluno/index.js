6c0e63b326b5b83e96b474443d7ec9c0b1822674
Arthur <arthurbiasibettifarias@gmail.com>
2019-07-15 01:06:55 -0300

    version 0.5

--- src/pages/Aluno/index.js
+++ src/pages/Aluno/index.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 
 import { View,Text, FlatList,  TouchableOpacity, Image, ScrollView, AsyncStorage } from 'react-native';
 
-import {NavigationAction} from 'react-navigation';
-
 import api from '../../services/api';
 
 import styles from './styles';
@@ -12,48 +10,69 @@ import logo from '../../assets/logo.png';
 
 
 
+
 export default class Aluno extends Component {
 
   state = {
-    turmas: [],
-    tarefas: [],
     dadosTarefa: []
   }
 
   async componentDidMount(){
     //pega turmas
-
+    dadosTarefas= []
+    turmas = []
+    tarefas = []
     const pessoa = await AsyncStorage.getItem('@athena:pessoa');
     const response = await api.get('turmas_aluno/'+pessoa);
 
-    response.data.forEach((data)=>{this.state.turmas.push(data.cod_turma)});
+    response.data.forEach((data)=>{turmas.push(data.cod_turma)});
     //pega tarefas
 
-    for(i=0; i<this.state.turmas.length;i++){
-      const response = await api.get('tarefas_turma/'+this.state.turmas[i]);
+    for(i=0; i<turmas.length;i++){
+      const response = await api.get('tarefas_turma/'+turmas[i]);
 
-      response.data.forEach((data)=>{this.state.tarefas.push(data.id_tarefa)});
+      response.data.forEach((data)=>{tarefas.push(data.id_tarefa)});
     }
     //pega dados tarefa
 
-    for(i=0; i<this.state.tarefas.length;i++){
-      const response = await api.get('tarefas/'+this.state.tarefas[i]);
+    for(i=0; i<tarefas.length;i++){
+      const response = await api.get('tarefas/'+tarefas[i]);
 
-      response.data.forEach((data)=>{this.state.dadosTarefa.push(data)});
+      response.data.forEach((data)=>{dadosTarefas.push(data)});
     }
+    
+    this.setState({dadosTarefa: dadosTarefas});
   }
-  render() {
-    return( 
-    <View style={styles.container}>
-      <View style={styles.topo}>
-        <Image style={styles.logo} source={logo}/>
+  renderItem = ({ item }) => (
+    <TouchableOpacity
+    onPress={() => {}}
+    style={styles.arquivo}
+    >
+      <View style={styles.fileInfo}>
+        <Text style={styles.fileTitle}>{item.nome_tarefa}</Text>
+        <Text style={styles.fileTitle}>Nota: {item.peso_nota}</Text>
       </View>
-      <FlatList
-        style={styles.list}
-        data={this.state.dadosTarefa}
+    </TouchableOpacity>
 
-      />     
-    </View>
-    );
-  }
-}
+  );
+
+  render() {
+      return( 
+      <View style={styles.container}>
+        <View style={styles.topo}>
+          <Image style={styles.logo} source={logo}/>
+        </View>
+        <FlatList
+          style={styles.list}
+          data={this.state.dadosTarefa}
+          keyExtractor={tarefa => tarefa.id_tarefa.toString()}
+          ItemSeparatorComponent={()=> <View style={styles.separator}/>}
+          renderItem={this.renderItem}
+        />
+        </View>
+      );      
+    }
+    }
+  
+
+
--- src/pages/Aluno/styles.js
+++ src/pages/Aluno/styles.js
@@ -1,28 +1,66 @@
-import { StyleSheet, Dimensions } from 'react-native';
-import { all } from 'rsvp';
+  
+import { StyleSheet, Platform, Dimensions } from "react-native";
+import { black } from "ansi-colors";
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#271B3D',
-        alignItems: 'stretch',
-        paddingRight: 20,
-    },
-    topo: {
-        backgroundColor:'#110C28',
-        width:Dimensions.get('window').width,
-        height: 120,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    aside: {
-        flex:2,
-        backgroundColor: '#382953',
-        width:Dimensions.get('window').width/2,
-    },
-    list: {
+  container: {
+    backgroundColor: '#271B3D',
+    paddingTop: Platform.OS === "ios" ? getStatusBarHeight() : 0,
+    flex: 1
+  },
 
-    }
+  boxTitle: {
+    marginTop: 50,
+    textAlign: "center",
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#333"
+  },
+
+  list: {
+    marginVertical: 30,
+    color: 'white',
+    },
+
+  file: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingVertical: 20
+  },
+
+  separator: {
+    height:25
+  },
+
+  fileInfo: {
+    width: Dimensions.get('window').width*0.95,
+    height: 120,
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: '#3A2046',
+    alignSelf:"center",
+  
+  },
+
+  fileTitle: {
+    fontSize: 16,
+    color: "white",
+    marginLeft: 10
+  },
+
+  fileDate: {
+    fontSize: 14,
+    color: "#666"
+  },
+
+  topo: {
+    backgroundColor:'#110C28',
+    width:Dimensions.get('window').width,
+    height: 120,
+    alignItems: 'center',
+    justifyContent: 'center'
+},
 });
 
 export default styles;
